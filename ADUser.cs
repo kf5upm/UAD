8b8d2f3fd862fb5c0cda60ee7facbc1aa7049862
M.Myers <M.Myers@KS-REPOANPR-1>
2023-08-31 10:34:21 -0500

    Add project files.

---
+++ ADUser.cs
@@ -0,0 +1,66 @@
+﻿using System.DirectoryServices;
+
+namespace User_Administration_Dashboard
+{
+    class ADUser
+    {
+        private DirectoryEntry _directoryEntry;
+
+        public ADUser(DirectoryEntry directoryEntry)
+        {
+            _directoryEntry = directoryEntry;
+        }
+
+        public string firstName
+        {
+            get { return _directoryEntry.Properties["givenname"].Value?.ToString(); }
+        }
+        public string lastName
+        {
+            get { return _directoryEntry.Properties["sn"].Value?.ToString(); }
+        }
+
+        public string emailAddress
+        {
+            get { return _directoryEntry.Properties["mail"].Value?.ToString(); }
+        }
+
+        public string city
+        {
+            get { return _directoryEntry.Properties["l"].Value?.ToString(); }
+        }
+
+        public string state
+        {
+            get { return _directoryEntry.Properties["st"].Value?.ToString(); }
+        }
+
+        public string title
+        {
+            get { return _directoryEntry.Properties["title"].Value?.ToString(); }
+        }
+        public string department
+        {
+            get { return _directoryEntry.Properties["department"].Value?.ToString(); }
+        }
+        public string manager
+        {
+            get {
+                string manager = _directoryEntry.Properties["manager"].Value?.ToString();
+                manager = manager.Split(',')[0];
+                manager = manager.Split('=')[1];
+                return manager;
+            }
+        }
+
+        public bool lockedout
+        {
+            get
+            {
+                string lockouttime = _directoryEntry.Properties["lockouttime"].Value?.ToString();
+
+                return lockouttime is null ? false : true;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.DirectoryServices;
+using System;
+using System.Diagnostics;
+
+namespace User_Administration_Dashboard
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string adminUser = "";
+        private static string adminPass = "";
+
+        private static DirectoryEntry _directoryEntry = new DirectoryEntry("", adminUser, adminPass);
+
+        public MainWindow()
+        {
+            _directoryEntry.AuthenticationType = AuthenticationTypes.Secure;
+
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            DirectorySearcher searcher = new DirectorySearcher(_directoryEntry);
+
+            string searchParameter = SearchParameter.Text;
+
+            searcher.Filter = $"(&(objectClass=User)(displayName={searchParameter}*))";
+
+            SearchResultCollection searchResults = searcher.FindAll();
+
+            if (searchResults.Count == 0)
+            {
+                MessageBox.Show("No users matching that search criteria were found.");
+            }
+            else if (searchResults.Count > 1)
+            {
+                foreach(SearchResult result in searchResults)
+                {
+                    DirectoryEntry de = result.GetDirectoryEntry();
+
+                    Debug.WriteLine(de.Name);
+                }
+
+                MessageBox.Show($"{searchResults.Count} resuls found");
+            }
+            else
+            {
+                DirectoryEntry de = searchResults[0].GetDirectoryEntry();
+
+                ADUser currentUser = new ADUser(de);
+
+                this.DataContext = currentUser;
+
+            }
+        }
+    }
+}
